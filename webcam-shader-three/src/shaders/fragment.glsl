c17ed235564b440264a5b4a557060689eb96824a
Root <inventoxz@gmail.com>
2023-09-15 14:21:42 +0200

    feat: glow effect, commented out

--- webcam-shader-three/src/shaders/fragment.glsl
+++ webcam-shader-three/src/shaders/fragment.glsl
@@ -2,5 +2,13 @@ uniform sampler2D textureUniform;
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor = texture2D(textureUniform, vUv);
+    vec4 texture = texture2D(textureUniform, vUv);
+    // float fac = .01;
+    // gl_FragColor = vec4(
+    //     fac / abs(texture.x),
+    //     fac / abs(texture.y),
+    //     fac / abs(texture.z),
+    //     1.
+    // );
+    gl_FragColor = texture;
 }
