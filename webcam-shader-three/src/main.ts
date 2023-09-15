00ae29561e36b2ee4a8ffde7d711ad6d5d0e18bb
Root <inventoxz@gmail.com>
2023-09-15 13:24:12 +0200

    feat: docs, resize logic

--- webcam-shader-three/src/main.ts
+++ webcam-shader-three/src/main.ts
@@ -12,14 +12,12 @@ camera.position.z = 0.01;
 
 const scene = new THREE.Scene();
 
+// texture
 const video = document.getElementById( 'video' ) as HTMLVideoElement ;
-
 const videoTexture = new THREE.VideoTexture( video );
 videoTexture.colorSpace = THREE.SRGBColorSpace;
 
-const geometry = new THREE.PlaneGeometry( 16, 9 );
-geometry.scale( 0.5, 0.5, 0.5 );
-
+// shaded material from video texture
 const videoShaderMaterial = new THREE.ShaderMaterial({
   vertexShader: vertex,
   fragmentShader: fragment,
@@ -28,41 +26,48 @@ const videoShaderMaterial = new THREE.ShaderMaterial({
   }
 });
 
+// plane mesh using material
+const geometry = new THREE.PlaneGeometry( 16, 9 );
+geometry.scale( 0.5, 0.5, 0.5 );
 const mesh = new THREE.Mesh( geometry, videoShaderMaterial );
 mesh.scale.multiply(new THREE.Vector3(-1, 1, 1))  // flip the mesh 180deg
 mesh.position.set(0, 0, -5)
 mesh.lookAt(camera.position)
 scene.add(mesh)
 
+// setup renderer
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// setup controls
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableZoom = true;
 controls.enablePan = true;
 
+// resize logic
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+})
+
+// request camera and set <video /> elem's src to webcam stream
 if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
-  // const constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
   const constraints = { video: { width: 1920, height: 1080, facingMode: 'user' } };
-  navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
-
-    // apply the stream to the video element used in the texture
-
-    video.srcObject = stream;
-    video.play();
-
-  } ).catch( function ( error ) {
-
-    console.error( 'Unable to access the camera/webcam.', error );
-
-  } );
-
+  navigator.mediaDevices.getUserMedia( constraints )
+    .then( function ( stream ) {
+      // apply the stream to the video element used in the texture
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch( function ( error ) {
+      console.error( 'Unable to access the camera/webcam.', error );
+    });
 } else {
-
   console.error( 'MediaDevices interface not available.' );
-
 }
 
 function animate() {
@@ -71,8 +76,10 @@ function animate() {
   if (video.readyState === video.HAVE_ENOUGH_DATA) {
     videoTexture.needsUpdate = true;
   }
+
   // update controls
   controls.update()
+
   // render
   renderer.render( scene, camera );
   requestAnimationFrame( animate );
